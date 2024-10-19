70380ab2b24a43778bbe1d748a21043a0beafa38
RUPAN C Varghese <rozaliacv32@gmail.com>
2024-10-19 15:35:37 +0530

    category table added

---
+++ backend/models/categories/init.js
@@ -0,0 +1,29 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../../config/database.config.js'; 
+const categories = sequelize.define('categories', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING(32),
+        allowNull: false
+    }
+}, {
+    tableName: 'categories',
+    timestamps: false  
+});
+
+
+const createCategoriesTable = async () => {
+    try {
+        await categories.sync({ alter: true });
+        console.log("categories table created or updated successfully");
+    } catch (error) {
+        console.error("Failed to create categories table: ", error.message);
+        throw new Error(error.message);
+    }
+};
+
+export default createCategoriesTable;
